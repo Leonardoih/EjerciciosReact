0f86b1497f52791ac39f5822425625ffd5e1566e
leonardoih <leonardoih090217@gamil.com>
2022-10-24 22:35:33 -0500

    Ejercicios de React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { log } from 'console';
+import React from 'react';
+
+function App() {
+	return (
+		<div>
+			Hola Mundo
+			{console.log('funcionando')}
+		</div>
+	);
+}
+
+export default App;
